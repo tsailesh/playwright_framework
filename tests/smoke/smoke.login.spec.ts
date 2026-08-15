ab8738ed9a50e9a61f031a49232d8b81fcc28608
tsailesh <ttsailesh@gmail.com>
2026-08-15 19:43:12 +0545

    Test fixes

---
+++ tests/smoke/smoke.login.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "../../src/fixtures/test.fixture";
+import logger from "../../src/utils/logger";
+
+test.describe(" @smoke Smoke: Login Test", () => {
+  test.beforeEach(async ({ loginPage }) => {
+    await loginPage.goto();
+    await expect(loginPage.usernameInput).toBeVisible();
+    await expect(loginPage.passwordInput).toBeVisible();
+    await expect(loginPage.loginButton).toBeVisible();
+  });
+
+  test("Login with valid credentials", async ({
+    loginPage,
+    dashboardPage,
+    page,
+  }) => {
+    await loginPage.login(
+      process.env.TEST_USERNAME!,
+      process.env.TEST_PASSWORD!,
+    );
+    const locator = await dashboardPage.getMessage();
+    await locator.isVisible();
+    logger.info(page.url());
+    expect(page.url()).toBe(`${process.env.BASE_URL}/secure`);
+  });
+
+  test("Login with invalid credentials", async ({ loginPage, page }) => {
+    await loginPage.login(
+      process.env.TEST_INVALID_USERNAME!,
+      process.env.TEST_PASSWORD!,
+    );
+    const locator = await loginPage.getMessage();
+    await locator.isVisible();
+    logger.info(page.url());
+    expect(page.url()).toBe(`${process.env.BASE_URL}/login`);
+  });
+});
