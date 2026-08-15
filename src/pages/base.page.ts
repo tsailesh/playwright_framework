9183f8529f53cafec027471dad5fe682c837d9cf
tsailesh <ttsailesh@gmail.com>
2026-08-15 14:17:42 +0545

    Base page added

---
+++ src/pages/base.page.ts
@@ -0,0 +1,21 @@
+import {Page} from "@playwright/test";
+
+class basePage{
+    protected page: Page;
+    constructor(page:Page) {
+        this.page = page;
+    }
+
+    async navigate(path:string){
+        await this.page.goto(path);
+    }
+
+    async waitForLoad(){
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async getTitle(){
+        return await this.page.title();
+    }
+
+}
