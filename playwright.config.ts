bddd0f97bf02ae9b23e9be211a7ce2ba8444ce3b
tsailesh <ttsailesh@gmail.com>
2026-08-15 13:56:14 +0545

    Staging environment added

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 /// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
+import {dotenv} from "dotenv";
+import {path} from "path";
 
 /**
  * Read environment variables from file.
@@ -23,14 +25,20 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'reports/html-report' }],
+    ['allure-playwright', { outputFolder: 'reports/allure-results' }],
+    ['list'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL||'https://practice.expandtesting.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
